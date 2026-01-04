b40ead19b37b67a6f3ed768ffe879fb0ed84982b
SamirAkhat7 <samir.akhtar444@gmail.com>
2026-01-05 00:31:02 +0530

    feat: Enable CORS support in the application

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,8 +1,25 @@
 import express from "express";
 import routes from "./routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+// app.use(cors()); // Allow all origins for now
+
+app.use(cors());
+
+// OR enable CORS for specific origins
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://https://d9vxjqxn-5173.inc1.devtunnels.ms/",
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true, // if sending cookies
+//   })
+// );
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
