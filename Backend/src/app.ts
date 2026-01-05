46ddafacb43d08a7ce0a33a3aa173147f1cc9204
Sohail3244 <sohailahmedmaniyar@gmail.com>
2026-01-05 12:21:05 +0530

    sohial5

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import routes from "./routes.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
